3a6d0a16716854fc299c7f1f3f2369bc0ab40b53
daniel <santoss.rdaniel@gmail.com>
2022-07-26 21:11:27 -0300

    [ADD] componente carousel

--- mechisjeans/src/App.js
+++ mechisjeans/src/App.js
@@ -1,11 +1,13 @@
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Header from './components/Header';
+import CarouselFadeExample from './components/CarouselFadeExample';
 
 function App() {
   return (
     <div className="App">
         < Header />
+        < CarouselFadeExample />
       
     </div>
   );
