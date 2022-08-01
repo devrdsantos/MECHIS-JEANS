bd10c7553e03d2e7d2876a4017331343c4bdfdd9
marco <massantemarcolucas@gmail.com>
2022-07-31 22:30:24 -0300

    [ADD] CardsShort

--- mechisjeans/src/App.js
+++ mechisjeans/src/App.js
@@ -5,10 +5,12 @@ import CarouselMain from './components/CarouselMain';
 import Footer from './components/Footer';
 import Cards from './components/Cards';
 import data  from './data';
+import dataShort  from './dataShort';
 import './styles/card.css';
 import RegistroDeIngreso from './components/LogInForm';
 import LogInForm from './components/LogInForm';
 import LogUpForm from './components/LogUpForm';
+import CardsShort from './components/CardsShort';
 
 function App() {
   return (
@@ -17,6 +19,8 @@ function App() {
         <CarouselMain />
         <Cards
         info = {data} />
+        <CardsShort
+        info = {dataShort} />
         <Footer />
     </div>
   );
---
+++ mechisjeans/src/dataShort.js
@@ -0,0 +1,53 @@
+import img1short from './assets/imgCards/PANTALÓN CORTO DE TENCEL CON COLOR.jpg';
+import img2short from './assets/imgCards/PANTALÓN CORTO PUSH IN SECRET GLAMOUR NEGRO.jpg';
+import img3short from './assets/imgCards/PANTALÓN VAQUERO PIRATA PUSH IN SECRET.jpg';
+import img4short from './assets/imgCards/PANTALONES CORTOS PUSH IN SECRET GLAMOUR CON COLOR amarillo.jpg';
+import img5short from './assets/imgCards/PANTALONES CORTOS PUSH IN SECRET GLAMOUR CON COLOR.jpg';
+import img6short from './assets/imgCards/short-denim-verano-2022-scaled.jpg';
+import img7short from './assets/imgCards/BERMUDA VAQUERA PUSH IN SECRET GLAMOUR CON LAVADO OSCURO.jpg';
+
+
+export const cardOne = {
+    imagen : img1short ,
+    titulo : 'TENCEL',
+    precio : 25
+}
+
+export const cardTwo = {
+    imagen : img2short ,
+    titulo : 'GLAMOUR NEGRO',
+    precio : 50
+}
+
+export const cardThree = {
+    imagen : img3short ,
+    titulo : 'PIRATA',
+    precio : 100
+}
+
+export const cardFour = {
+    imagen : img4short ,
+    titulo : 'PUSH IN SECRET',
+    precio : 25
+}
+
+export const cardFive = {
+    imagen : img5short ,
+    titulo : 'PUSH IN SECRET',
+    precio : 50
+}
+
+export const cardSix = {
+    imagen : img6short ,
+    titulo : 'DENIN',
+    precio : 100
+}
+
+export const cardSeven = {
+    imagen : img7short ,
+    titulo : 'VAQUERA',
+    precio : 100
+}
+
+let imagenesSlide = [cardOne, cardTwo, cardThree, cardFour, cardFive, cardSix, cardSeven];
+export default imagenesSlide;
